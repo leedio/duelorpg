05af9e36f4aa217a2e7415abcbf35fd70b6a3e15
leedio <leedio@msn.com>
2023-08-25 16:15:55 -0300

    v1.1

--- src/Jogo/test/PersonagemTest.java
+++ src/Jogo/test/PersonagemTest.java
@@ -9,14 +9,14 @@ public class PersonagemTest {
        
     
 
-        Personagem twitch = new Personagem("Twitch", 115, 27, 682,11);
+        Personagem twitch = new Personagem("Twitch", 105, 25, 602,11);
         Personagem ashe = new Personagem("Ashe", 105, 26, 640,9);
-        Personagem teemo = new Personagem("Teemo", 94, 24, 598,9);
-        Personagem garen = new Personagem("Garen", 90, 40, 690,6);
+        Personagem teemo = new Personagem("Teemo", 94, 34, 598,10);
+        Personagem garen = new Personagem("Garen", 90, 40, 850,7);
         Personagem talon = new Personagem("Talon", 108, 35, 658,7);
-        Personagem alistar= new Personagem("Alistar", 70, 55, 758,8);
-        Personagem fizz = new Personagem("Fizz", 95, 30, 600,13);
-        Personagem gnar = new Personagem("Gnar", 89, 30, 858,8);
+        Personagem alistar= new Personagem("Alistar", 70, 55, 858,8);
+        Personagem fizz = new Personagem("Fizz", 85, 30, 600,12);
+        Personagem gnar = new Personagem("Gnar", 89, 30, 900,8);
 
        //twitch.atacar(talon);
 
